c83c34aeadec86d7ebdfaff2aba0d6095996d233
Valentin Rangelov <eneragee@gnail.com>
2017-08-19 11:59:24 +0100

    Added words.xml file with all the globish words with description to them in the Resources folder.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strArr = new List<string>();
+
+            using (var sr = new StreamReader(@"C:\Users\enera\Desktop\SimpleGlobish\ConsoleApplication1\ConsoleApplication1\New Text Document.txt"))
+            {
+                var count = 0;
+                while (true)
+                {
+                    if (count == 1501)
+                    {
+                        break;
+                    }
+                    count++;
+                    strArr.Add(sr.ReadLine());
+                }
+            }
+
+            using (var sr = new StreamWriter(@"C:\Users\enera\Desktop\SimpleGlobish\ConsoleApplication1\ConsoleApplication1\New Text Document (2).txt"))
+            {
+                foreach (var item in strArr)
+                {
+                    var currentSplittedItem = item.Split(' ');
+                    var key = currentSplittedItem[0];
+                    string value = string.Empty;
+                    for (int i = 1; i < currentSplittedItem.Length; i++)
+                    {
+                        value += currentSplittedItem[i] + " ";
+                    }
+                    value = value.TrimEnd();
+
+                    sr.WriteLine(string.Format("<item name=\"{0}\">{1}</item>", key, value));
+                }
+            }
+        }
+    }
+}
